0c98e60caae5b313103fcb27d32ab349e66f4b32
sharova <sv_sharova@mail.ru>
2018-12-13 21:28:48 +0300

    index-css

--- src/js/modules/bookCardTemplate.js
+++ src/js/modules/bookCardTemplate.js
@@ -4,7 +4,7 @@ const bookCardTemplate = {
   tagClass: 'product-card-mini',
   setContent: function(book) {
     return `<h2 class="product-card-mini__title"><a href="${book.titleLink}">${book.name}</a></h2>
-          <a href="${book.imgWrap}" class="product-card-mini__img-wrap"><img src="${book.imgLink}" alt="" class="product-card-mini__img"></a>
+          <a href="${book.imgWrap}" class="product-card-mini__img-wrap"><img src="img/books_all/${book.uri}.png" alt="" class="product-card-mini__img"></a>
           <p class="product-card-mini__descr">${book.desc}</p>
           <div class="product-card-mini__price">${book.price}</div>`;
   }
