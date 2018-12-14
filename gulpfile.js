2cdde4142784158a0272fe2b5625f173a04394ab
sharova <sv_sharova@mail.ru>
2018-12-14 21:47:54 +0300

    Добавлен деплой

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,9 @@ const rename = require('gulp-rename');
 const webpack = require('webpack');
 const webpackStream = require('webpack-stream');
 
+const ghpages = require('gh-pages');
+const path = require('path');
+
 function styles() {
   return src(`${dir.src}scss/style.scss`)
     .pipe(plumber())
@@ -89,6 +92,11 @@ function clean() {
   return del(dir.build)
 }
 exports.clean = clean;
+
+function deploy(cb) {
+  ghpages.publish(path.join(process.cwd(), dir.build), cb);
+}
+exports.deploy = deploy;
 
 function serve() {
   browserSync.init({
