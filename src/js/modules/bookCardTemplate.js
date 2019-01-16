fc11a045af9dfc61be59e9a5677ff04d26b0abea
sharova <sv_sharova@mail.ru>
2019-01-16 23:09:34 +0300

    Исправление ошибок

--- src/js/modules/bookCardTemplate.js
+++ src/js/modules/bookCardTemplate.js
@@ -3,10 +3,10 @@ const bookCardTemplate = {
   tag: 'article',
   tagClass: 'product-card-mini',
   setContent: function(book) {
-    return `<h2 class="product-card-mini__title"><a href="${book.titleLink}">${book.name}</a></h2>
+    return `<div class="product-card-mini__wrap"><h2 class="product-card-mini__title"><a href="${book.titleLink}">${book.name}</a></h2>
           <a href="${book.imgWrap}" class="product-card-mini__img-wrap"><img src="img/books_all/${book.uri}.png" alt="" class="product-card-mini__img"></a>
           <p class="product-card-mini__descr">${book.desc}</p>
-          <div class="product-card-mini__price">${book.price} ₽</div>`;
+          <div class="product-card-mini__price">${book.price} ₽</div></div>`;
   }
 }
 
